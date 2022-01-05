ab79251f106e0ea7fd2eab2bb4d5234764ed4d0c
AgrielMajere <live.cibum@gmail.com>
2022-01-05 18:44:15 +0200

    рендер страницы

---
+++ src/js/changeTheme.js
@@ -0,0 +1,37 @@
+import createMenuMarkup from '../templates/menuCards.hbs';
+import menu from '../data/menu.json';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+  menuList: document.querySelector('.js-menu'),
+  switcher: document.querySelector('#theme-switch-toggle'),
+  body: document.body,
+};
+
+refs.menuList.innerHTML = createMenuMarkup(menu);
+
+refs.switcher.addEventListener('change', changeTheme);
+
+function changeTheme(event) {
+  if (event.target.checked) {
+    refs.body.classList.add(Theme.DARK);
+    refs.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    refs.body.classList.remove(Theme.DARK);
+    refs.body.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+// function themeDefault() {
+//   if (refs.body.classList.contains(Theme.DARK)) {
+//     refs.switcher.checked = true;
+//   } else {
+//     refs.switcher.checked = false;
+//   }
+// }
