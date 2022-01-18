707e5f81df51cddf2dbd62f33754cb7634131bb8
AgrielMajere <87906683+AgrielMajere@users.noreply.github.com>
2022-01-18 22:33:44 +0200

    getting theme from local storage

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -27,6 +27,11 @@ function changeTheme(event) {
     localStorage.setItem('theme', Theme.LIGHT);
   }
 }
+(function(){
+refs.body.classList.add(localStorage.getItem('theme')?localStorage.getItem('theme'):Theme.LIGHT,
+);
+refs.switcher.checked = localStorage.getItem('theme') === Theme.DARK;
+})()
 
 // function themeDefault() {
 //   if (refs.body.classList.contains(Theme.DARK)) {
